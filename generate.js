9d2732f1ea2de4d0b39d8be6caac60e003ee1719
Mohammad Khanafi <mohammad_khanafi@smltech.co.id>
2024-12-10 23:26:58 +0700

    mockup table with dynamic column. one label column static, and dynamic column

--- generate.js
+++ generate.js
@@ -1,17 +1,40 @@
-module.exports = function() {
-    const faker = require("faker");
-    const _ = require("lodash");
-    return {
-        blogPosts: _.times(5, function (n) {
-            return {
-                id: n,
-                category: faker.helpers.randomize(["Science", "Technology", "Religion", "Politics", "Movies"]),
-                postDate: faker.date.recent(),
-                authorName: faker.name.findName(),
-                authorEmailAddress: faker.internet.email(),
-                subject: faker.hacker.phrase(),
-                postText: faker.lorem.paragraphs(4)
-            }
-        })
+const faker = require("faker");
+const fs = require("fs");
+const _ = require("lodash");
+
+const inputs = [];
+const outputs = [];
+
+for (let i = 1; i <= 10; i++) {
+  const input = {
+    id: i,
+    category: faker.helpers.randomize(["Science", "Technology", "Religion", "Politics", "Movies"]),
+    postDate: faker.date.recent(),
+    authorName: faker.name.findName(),
+    authorEmailAddress: faker.internet.email(),
+    subject: faker.hacker.phrase(),
+    postText: faker.lorem.paragraphs(4),
+  };
+
+  inputs.push(input)
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    const output = {
+      id: i,
+      category: faker.helpers.randomize(["Science", "Technology", "Religion", "Politics", "Movies"]),
+      postDate: faker.date.recent(),
+      authorName: faker.name.findName(),
+      authorEmailAddress: faker.internet.email(),
+      subject: faker.hacker.phrase(),
+      postText: faker.lorem.paragraphs(4),
     };
-};
+  
+    outputs.push(output)
+  }
+
+  fs.writeFileSync('scenario.json', JSON.stringify({
+        inputs,
+        outputs,
+    },null,2))
--- src/App.js
+++ src/App.js
@@ -1,48 +1,80 @@
-import React, {Component} from 'react';
-import './App.css';
-import Axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import Axios from "axios";
 
+const labelData = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {data: []};
+    this.state = { data: [] , outputs:[]};
   }
   componentDidMount() {
-    Axios.get("http://localhost:3003/blogPosts")
-      .then( response => {
-        this.setState({data: response.data})});
+    Axios.get("http://localhost:3001/inputs").then((response) => {
+      this.setState({ data: response.data });
+    });
+    Axios.get("http://localhost:3001/outputs").then((response) => {
+      this.setState({ outputs: response.data });
+    });
   }
 
-  render(){
-    if(this.state && this.state.data){
+  render() {
+    if (this.state && this.state.data) {
       console.log(this.state.data);
-      const dataTable = this.state.data.map( (blogPost, i) => {
-        return <tr>
-                <td>{blogPost.category}</td>
-                <td>{blogPost.postDate}</td>
-                <td><a href="mailto: {blogPost.authorEmailAddress}"></a>{blogPost.authorName}</td>
-                <td>{blogPost.subject}</td>
-               </tr>
+      const dataTable = this.state.data.map((data, i) => {
+        return (
+          <>
+            <td>{data.category}</td>
+            <td>{data.postDate}</td>
+            <td>
+              <a href="mailto: {blogPost.authorEmailAddress}"></a>
+              {data.authorName}
+            </td>
+            <td>{data.subject}</td>
+          </>
+        );
       });
-      return <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Category</th>
-              <th>Date</th>
-              <th>Author</th>
-              <th>Subject</th>
-            </tr>
-          </thead>
-          <tbody>
-            {dataTable}
-          </tbody>
-        </table>
-      </div>;
+
+      const d = this.state.data;
+      const c = this.state.outputs;
+
+      const LabelHeader = labelData.map((data, i) => {
+        return (
+          <tr key={i}>
+            <td>{data}</td>
+            <td> {d[i]?.category}</td>
+            <td> {d[i]?.postDate}</td>
+            <td> {d[i]?.authorName}</td>
+            <td> {d[i]?.subject}</td>
+            <td> {c[i]?.category}</td>
+            <td> {c[i]?.postDate}</td>
+            <td> {c[i]?.authorName}</td>
+            <td> {c[i]?.subject}</td>
+          </tr>
+        );
+      });
+      return (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Label</th>
+                <th>sc 1</th>
+                <th>sc 1</th>
+                <th>sc 1</th>
+                <th>sc 1</th>
+                <th>sc 1</th>
+                <th>sc 1</th>
+                <th>sc 1</th>
+                <th>sc 1</th>
+              </tr>
+            </thead>
+            <tbody>{LabelHeader}</tbody>
+          </table>
+        </div>
+      );
     }
     return null;
-    
   }
 }
 
